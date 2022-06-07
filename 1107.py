8cbbc300f7efcc9606ab6729772466d9829c52d9
miunae <61579596+miunae@users.noreply.github.com>
2022-06-07 17:10:50 +0900

    BOJ_1107
    [백준] 1107번: 리모컨

---
+++ 1107.py
@@ -0,0 +1,18 @@
+target = int(input())  # 목표 채널
+M = int(input())  # 고장 버튼 수
+answer = abs(100-target)  # 현재 채널에서 +,- 만 이용할 때
+if M:
+    out = list(map(int,input().split()))  # 고장난 버튼
+else:
+    out = []
+
+for nums in range(1000001):
+    nums = str(nums)
+    for i in range(len(nums)):
+        if int(nums[i]) in out:
+            break
+
+        elif i == len(nums)-1:
+            answer = min(answer, abs(int(nums)-target)+len(nums))
+
+print(answer)
